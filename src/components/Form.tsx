668ae2325ba3f1978fb0ae5d7088591f6ce0b268
Haydn <lizexu@dhcp-128-189-83-125.ubcsecure.wireless.ubc.ca>
2023-05-04 16:01:51 -0700

    changing the category into select component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,42 +1,68 @@
-import React, { FormEvent, useRef, useState } from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import { appendErrors, FieldValues, useForm } from 'react-hook-form'
+
+interface FormData {
+    description: string;
+    amount: string;
+    category: number;
+}
 
 const Form = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    console.log(errors)
 
-    const [person, setPerson] = useState({
-        description: "",
-        amount: "",
-        category: 0,
-    });
+    const onSubmit = (data: FieldValues) => console.log(data);
 
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(person);
-        console.log('Submitted!!');
-    }
+    // const [person, setPerson] = useState({
+    //     description: "",
+    //     amount: "",
+    //     category: 0,
+    // });
+
+    // const handleSubmit = (event: FormEvent) => {
+    //     event.preventDefault();
+    //     // console.log(person);
+    //     console.log('Submitted!!');
+    // }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor='description' className="form-label">Description</label>
-                <input value={person.description} onChange={(event) => {
-                    setPerson({ ...person, description: event.target.value })
-                }} id="description" type="text" className="form-control" />
+                <input
+                    {...register('description', { required: true, minLength: 3 })}
+                    // value={person.description}
+                    // onChange={(event) => {
+                    //     setPerson({ ...person, description: event.target.value })
+                    // }}
+                    id="description" type="text" className="form-control" />
             </div>
+            {errors.description?.type === 'required' && <p className='text-danger'>The name is required</p>}
+            {errors.description?.type === 'minLength' && <p className='text-danger'>The description must be at least 3 characters long</p>}
+
 
             <div className="mb-3">
                 <label htmlFor="amount" className="form-label">Amount</label>
-                <input value={person.amount} onChange={(event) => {
-                    setPerson({ ...person, amount: event.target.value })
-                }} id="amount" type="text" className="form-control" />
+                <input
+                    {...register('amount')}
+                    // value={person.amount}
+                    // onChange={(event) => {
+                    //     setPerson({ ...person, amount: event.target.value })
+                    // }}
+                    id="amount" type="text" className="form-control" />
             </div>
 
             <div className="mb-3">
-                <label htmlFor="category" className="form-label">Category</label>
-                <input value={person.category} onChange={(event) => {
-                    setPerson({ ...person, category: parseInt(event.target.value) })
-                }}
-                    id="category" type="number" className="form-control" />
+                <label htmlFor="category" className='form-label'>Catrgory</label>
+                <select
+                    {...register('category')}
+                    id='category' className='form-control'
+                >
+                    <option value="milk">Milk</option>
+                    <option value="option2">Option2</option>
+                    <option value="option3">Option3</option>
+                </select>
             </div>
 
             <button className="btn btn-primary" type="submit">Submit</button>
